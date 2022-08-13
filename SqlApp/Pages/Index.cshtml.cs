6340429ee4caf9bc66c6e0a37d7848071d4b94a4
2194217 <2194217@LTIN232047>
2022-08-13 14:37:55 +0530

    Add project files.

---
+++ SqlApp/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SqlApp.Models;
+using SqlApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqlApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductService productservice = new ProductService();
+            Products = productservice.GetProducts();
+
+        }
+    }
+}
---
+++ SqlApp/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using SqlApp.Models;
+
+namespace SqlApp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "saikattestazuredbserver.database.windows.net";
+        private static string db_user = "sqladmin";
+        private static string db_password = "Azpassword9093*";
+        private static string db_database = "azuredb";
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+
+            List<Product> _product_list = new List<Product>();
+
+            string statement = "Select ProductID, ProductName, Quantity from Products";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(statement, conn);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        ProductID = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+
+                    };
+                    _product_list.Add(product);
+                }
+            }
+            conn.Close();
+            return _product_list;
+        }
+    }
+}
